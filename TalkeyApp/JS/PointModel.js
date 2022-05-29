8deaee08f0519eb1d0a53e2204c7df3ee686c231
Maria João Gomes <92551145+Mariaj17@users.noreply.github.com>
2022-05-29 18:35:26 +0100

    Add files via upload

---
+++ TalkeyApp/JS/PointModel.js
@@ -0,0 +1,12 @@
+class Points {
+    id=0;
+    username = "";
+    points = 0;
+
+    constructor(id, username, points) {
+      this.id = id;
+      this.username = username;
+      this.points = points;
+    }
+}
+
