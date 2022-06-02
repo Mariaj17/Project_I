9639bfc8bcfc6463c140c7dee489645dbe77966f
40210465 <40210465@esmad,ipp.pt>
2022-06-02 04:48:12 +0100

    landing page desktop finished

---
+++ TalkeyApp/JS/index.js
@@ -0,0 +1,19 @@
+// selecionar todos os headers do acordeão
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+// para alterar o ícone da seta de forma dinámica no toogle no evento clique
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener('click', event => {
+        accordionItemHeader.classList.toggle("active")
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        // se conter a class active 
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }else{
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
+
+
+
