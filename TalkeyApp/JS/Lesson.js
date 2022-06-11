8f1a5e815cbe220afdb13c616702576bdc1d7517
40210465 <40210465@esmad,ipp.pt>
2022-06-11 21:15:04 +0100

    lesson update html css js

--- TalkeyApp/JS/Lesson.js
+++ TalkeyApp/JS/Lesson.js
@@ -16,3 +16,50 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 })
 
 
+// -----------------------------------PARA OS COMENTÁRIOS-------------------------------
+const commentField = document.querySelector('#comment');
+const backUp = commentField.getAttribute('placeholder');
+const btn = document.querySelector('.btns');
+const clear = document.getElementById('cancelbtn');
+const cancelBtn = document.querySelector('#cancelbtn');
+const commentBtn = document.querySelector('#commentbtn');
+
+
+commentField.onfocus = function(){
+    this.setAttribute('placeholder','');
+    this.style.borderColor = '#005086';
+    btn.style.opacity = '1';
+    cancelBtn.style.cursor = 'pointer';
+    commentBtn.style.cursor = 'pointer';
+}
+
+commentField.onblur = function(){
+    this.setAttribute('placeholder',backUp);
+    this.style.borderColor = '#00508633';
+}
+
+clear.onclick = function(){
+    btn.style.opacity = '0';
+    commentField.value = '';
+}
+
+// FUNÇÃO PARA CONTAR O TEMPO DO COMENTÁRIO
+
+let comments = document.querySelector('.add-comments-container');
+// PARA ADICIONAR OS COMENTÁRIOS NO CLIQUE DO BOTÃO "COMENTAR"
+// A SRC DO AVATAR IMG VEM DA LOCALSTORAGE IMG ID="USER AVATAR" SRC="${}" => AVATAR DO USER
+// O NOME DO USER VEM DA LOCALSTORAGE <span id="username">${}</span>
+document.querySelector('#commentbtn').addEventListener('click',()=>{
+    // CommentInput = armazena o contéudo do comentário
+    const commentInput = document.querySelector(".commentUser").value;
+  comments.innerHTML += `  <div class="addUserComment">
+  <div class="addUsercontentComment">
+  <img id="userAvatar" src="/Media/images/avatar01.png" alt="#"><span id="username">Username</span>
+    <div class="flex-container">
+  <span id="timeComment">há min</span>
+    <div class="commentContent">${commentInput}</div>
+    </div>
+  </div>  
+  </div>
+  ` 
+})
