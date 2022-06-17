a85861b1b5a1069a63f804337d6b8e769b5e2a7b
Maria Joao Gomes <mariajoao.mjg17@gmail.com>
2022-06-17 15:17:41 +0100

    update

--- TalkeyApp/JS/Models/LessonModal.js
+++ TalkeyApp/JS/Models/LessonModal.js
@@ -44,3 +44,15 @@ class Level{
         this.classes = classes;
     }
 }
+
+class Points {
+    id=0;
+    username = "";
+    points = 0;
+
+    constructor(id, username, points) {
+      this.id = id;
+      this.username = username;
+      this.points = points;
+    }
+}
