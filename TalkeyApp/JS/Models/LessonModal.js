226aae7e00115f82b840fa8349772c14f3132910
Maria Joao Gomes <mariajoao.mjg17@gmail.com>
2022-06-17 15:05:19 +0100

    update

--- TalkeyApp/JS/Models/LessonModal.js
+++ TalkeyApp/JS/Models/LessonModal.js
@@ -1,7 +1,46 @@
-let lessons;
+let levels;
 
 // CARREGAR lessons DA LOCALSTORAGE
 export function init() {
-  lessons = localStorage.lessons ? JSON.parse(localStorage.lessons) : [];
+  levels = localStorage.levels ? JSON.parse(localStorage.levels) : [];
 }
+  
+  // DEFINIR O LEVEL ATUAL
+  export function setCurrentLevel(id) {
+    localStorage.setItem("level", id);
+  }
+  
+  // OBTER O LEVEL ATUAL
+  export function getCurrentLevel() {
+    return level.find((level) => level.id === localStorage.getItem("level"));
+  }
+  
 
+class Level{
+    id= 1;
+    classes= [
+          {
+            id: 1,
+            img:"",
+            title:"",
+            description:"",
+        },
+        {
+          id: 2,
+          img:"",
+          title:"",
+          description:"",
+        },
+        {
+          id: 3,
+          img:"",
+          title:"",
+          description:"",
+        },
+    ]
+
+    constructor(id,classes) {
+        this.id = id;
+        this.classes = classes;
+    }
+}
